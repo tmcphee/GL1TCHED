f6e6639ee4dcd7a77e9df73c5acac4273e4ba651
aargreer <aagreer@lakeheadu.ca>
2021-01-19 18:30:18 -0500

    skybox script
    added a colour changer script for changing the skybox background slightyl by a random amount every frame

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    Color c;
+
+    void Start()
+    {
+        c = GetComponent<Light>().color;
+    }
+
+    void Update()
+    {
+        //generates a random vector and slightly varies the background colour from fram eto fram
+        Color mutator = new Vector4(RandomFloat(10000f), RandomFloat(10000f), RandomFloat(10000f), 1f);
+        c += mutator * Time.deltaTime;
+    }
+
+    //Random.Range() except it returns a random float between -0.5f and 0.5f (Works best with large values of max)
+    float RandomFloat(float max) { return ((Random.Range(0, max) - (0.5f * max)) / max); }
+}
