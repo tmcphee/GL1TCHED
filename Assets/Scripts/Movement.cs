06e8a0c6654a770319e4f54f43890d5b5afe4cd8
aargreer <aagreer@lakeheadu.ca>
2021-01-27 20:30:49 -0500

    movement
    added some simple movement and updated the sample scene. also include experimental collision handling

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D r;
+    public float magnitude;
+    public float topSpeed;
+    private Vector2 v;
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = new Vector2(Input.GetAxis("Horizontal") * magnitude, Mathf.Abs(Input.GetAxis("Vertical") * 2 * magnitude));
+
+        if (r.rotation != 0)
+        {
+            r.MoveRotation(Mathf.Abs(r.rotation - 1f) + Random.Range(0, (r.velocity.x/topSpeed)*5));
+        }
+
+        if (Mathf.Abs(r.velocity.x) < topSpeed)
+        {
+            r.AddForce(v, ForceMode2D.Impulse);
+        }
+    }
+}
