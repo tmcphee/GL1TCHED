95e2bfa2638870b1dbe7b3cdc9ae248e7217c14a
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2021-02-01 16:31:55 -0500

    Added use checkpoint glitch and get last position

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private Rigidbody2D rb2D;
+    public bool UseCheckpointGlitch = false;
     public GameObject[] checkpoints;
-
+    public GameObject FinishCheckpoint;
+    
     private static string savefile = System.IO.Directory.GetCurrentDirectory() + "\\PlayerSave.json";
     private static playersave psave;
 
 
     public class playersave
     {
-        private int world = 0;
-        private int level = 0;
-        private int checkpoint = 0;
+        public int world = 0;
+        public int level = 0;
+        public int checkpoint = 0;
 
         public playersave()
         {
@@ -43,6 +44,23 @@ public class Checkpoint : MonoBehaviour
             this.checkpoint++;
             save();
         }
+        public void incrementLevel()
+        {
+            this.level++;
+            save();
+        }
+        public void incrementWorld()
+        {
+            this.world++;
+            save();
+        }
+        public void resetdata()
+        {
+            this.world = 0;
+            this.level = 0;
+            this.checkpoint = 0;
+            save();
+        }
 
         //Returns Level data
         public int getWorld()
@@ -57,7 +75,6 @@ public class Checkpoint : MonoBehaviour
         {
             return this.checkpoint;
         }
-
 
         //Loads level data from file
         public void load()
@@ -80,6 +97,7 @@ public class Checkpoint : MonoBehaviour
 
             using (System.IO.StreamWriter sw = System.IO.File.CreateText(savefile))
             {
+                Debug.Log("SAVE: " + json);
                 sw.Write(json);
                 sw.Close();
             }
@@ -101,15 +119,27 @@ public class Checkpoint : MonoBehaviour
         return -1;
     }
 
+    //Gets the position of the last checkpoint
+    public Vector3 GetLastCheckpointPosition()
+    {
+        //Make sure the last checkpoint is in range of array
+        int lastcheckpoint = psave.getCheckpoint();
+        if (lastcheckpoint > checkpoints.Length)
+        {
+            lastcheckpoint = checkpoints.Length - 1;
+        }
+        return checkpoints[lastcheckpoint].transform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         psave = new playersave();
         psave.load();
         
-
         Debug.Log("Start");
 
+        //Loop though all the defined checkpoint objects and add a BoxCollider2D with isTrigger set
         foreach(GameObject obj in checkpoints)
         {
             if (obj != null)
@@ -118,18 +148,44 @@ public class Checkpoint : MonoBehaviour
                 c.isTrigger = true;
             }
         }
-        
+
+        //adds a BoxCollider2D with isTrigger set to the finished checkpoint if exists
+        if (FinishCheckpoint != null)
+        {
+            BoxCollider2D fc = FinishCheckpoint.AddComponent<BoxCollider2D>() as BoxCollider2D;
+            fc.isTrigger = true;
+        }
+
+        psave.resetdata();
     }
 
     //Checks if the player touched a checkpoint
     void OnTriggerEnter2D(Collider2D Collider)
     {
+        if(Collider.gameObject == FinishCheckpoint)
+        {
+            Debug.Log("Player Finished Level");
+            psave.incrementLevel();
+            psave.setCheckpoint(0);
+            return;
+        }
+
         foreach (GameObject obj in checkpoints)
         {
             if (Collider.gameObject == obj)
             {
-                Debug.Log("Collison Checkpoint: " + find_checkpoint_index(obj) + "\nCheckpoint Data: " + psave.getCheckpoint());
-                psave.incrementCheckpoint();
+                int hit_checkpoint = find_checkpoint_index(obj);
+                int last_checkpoint = psave.getCheckpoint();
+                if (UseCheckpointGlitch)//If defined then increment checkpoint else only increment if checkpoint is the next
+                {
+                    psave.incrementCheckpoint();
+                }
+                else if (hit_checkpoint > last_checkpoint)
+                {
+                    psave.setCheckpoint(hit_checkpoint);
+                }
+                Debug.Log("Hit Checkpoint: " + find_checkpoint_index(obj));
+                return;
             }
         }
     }
