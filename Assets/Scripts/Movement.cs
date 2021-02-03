d7279abc45fd66766849d835692b8eb0d5e19f59
aargreer <aagreer@lakeheadu.ca>
2021-02-03 15:31:20 -0500

    movement physics
    increased physics tickrate for more consistent glitching; added a toggle for infinite jump + a check if the player is on the ground; created prefabs for ground + glitchable wall; camera now has a wider field-of-view; small movement and collision tweaks

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,23 +7,43 @@ public class Movement : MonoBehaviour
     public Rigidbody2D r;
     public float magnitude;
     public float topSpeed;
+    public bool infiniteJump;
+    public GameObject ground;
+    private bool onGround = false;
+
+    //checks if player is touching the ground object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.Equals(ground))
+        {
+            onGround = true;
+        }
+        else onGround = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //self-righting force; returns player to vertical by 0.75 degrees/frame
         if (r.rotation != 0)
         {
-            r.MoveRotation(Mathf.Abs(r.rotation - 0.5f));
+            r.MoveRotation(Mathf.Abs(r.rotation - 0.75f));
         }
 
+        //applies force if player hasn't exceeded top speed
         if (Mathf.Abs(r.velocity.x) < topSpeed)
         {
             r.AddForce(new Vector2(Input.GetAxis("Horizontal") * magnitude * 1.75f, 1f));
         }
 
+        //checks if either player is on the ground or infiniteJump is active
         if (Input.GetButtonDown("Jump"))
         {
-            r.AddForce(new Vector2(1f, magnitude * 2.5f), ForceMode2D.Impulse);
+            if(onGround || infiniteJump)
+            {
+                onGround = false;
+                r.AddForce(new Vector2(1f, magnitude * 1.75f), ForceMode2D.Impulse);
+            }
         }
     }
 }
