b10db379d3c9c41f158fd5af7d74b79bd883ec67
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2021-02-06 13:12:25 -0500

    Added In Game Menu with working "Restart from last checkpoint"

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour
+{
+
+    public GameObject InGameMenuCanvas;
+    //public Scene GameScene;
+    public GameObject PlayerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InGameMenuCanvas.SetActive(false);
+    }
+
+    public void RestartFromLastCheckpoint_Click()
+    {
+        PlayerObject.GetComponent<Movement>().SetPlayerLastCheckpoint();
+        InGameMenuCanvas.SetActive(false);
+    }
+
+    void OnEsc_Click()
+    {
+        InGameMenuCanvas.SetActive(true);
+    }
+
+    public void BackBtn_Click()
+    {
+        InGameMenuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            OnEsc_Click();
+        }
+    }
+}
