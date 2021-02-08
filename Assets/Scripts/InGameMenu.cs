55fd6fcf0793e229260d278f0e6f096fc199e7ca
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2021-02-07 21:49:41 -0500

    Modified player when In game menu is open
    Added freezing player when menu is open and resuming when menu is closed. Saves and resumes player velocity.

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class InGameMenu : MonoBehaviour
 {
+    private Vector3 playervelocity = new Vector3(0, 0, 0);
+    private float gravityscale;
 
     public GameObject InGameMenuCanvas;
+    public GameObject r;
+
+    //Disable Movment of player by disableing the object
+    //Saves player velocity and gravity
+    private void freezeplayer()
+    {
+        playervelocity = r.GetComponent<Rigidbody2D>().velocity;
+        gravityscale = r.GetComponent<Rigidbody2D>().gravityScale;
+        r.SetActive(false);
+    }
+
+    //Enables Movment of player by enableing the object
+    //Restores player velocity and gravity
+    private void unfreezeplayer()
+    {
+        r.SetActive(true);
+        r.GetComponent<Rigidbody2D>().velocity = playervelocity;
+        r.GetComponent<Rigidbody2D>().gravityScale = gravityscale;
+        
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -13,19 +35,26 @@ public class InGameMenu : MonoBehaviour
         InGameMenuCanvas.SetActive(false);
     }
 
+    //Sets the player to its last checkpoint
     public void RestartFromLastCheckpoint_Click()
     {
+        unfreezeplayer();
         this.GetComponent<Checkpoint>().SetPlayerLastCheckpoint();
         InGameMenuCanvas.SetActive(false);
     }
 
+    //On Menu Enter freese player and show menu
+    //On Menu Enter freese player and show menu
     void OnEsc_Click()
     {
+        freezeplayer();
         InGameMenuCanvas.SetActive(true);
     }
 
+    //closes the Menu
     public void BackBtn_Click()
     {
+        unfreezeplayer();
         InGameMenuCanvas.SetActive(false);
     }
 
