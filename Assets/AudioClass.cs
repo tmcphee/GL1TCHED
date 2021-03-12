a4d6a9e1085a9815869c55f36d918de578d65b68
aargreer <aagreer@lakeheadu.ca>
2021-03-12 16:31:16 -0500

    music update + hint updates
    also fixed volume slider settings

---
+++ Assets/AudioClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClass : MonoBehaviour
+{
+    private AudioSource music;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject.transform);
+        music = GameObject.Find("Music Source").GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (music.isPlaying) return;
+        music.Play();
+    }
+
+    public void StopMusic()
+    {
+        music.Stop();
+    }
+}
