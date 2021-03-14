95d546e3d8f929c32cf8c85e8dbee95013a5ef5d
aargreer <aagreer@lakeheadu.ca>
2021-03-13 20:31:38 -0500

    Revert "Revert "Fixed audio bug, removed redundant example levels""
    This reverts commit 69d134f011efa1933f3de53b8bcabdda64a5822f.

--- Assets/AudioClass.cs
+++ Assets/AudioClass.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* Andrew Greer
+ *  - gets the music audio source object and provides methods for playing and stopping music
+ *      - the DontDestroyOnLoad on this object allows the music to continue playing when a new scene is loaded
+ */
+
 public class AudioClass : MonoBehaviour
 {
     private AudioSource music;
@@ -9,13 +15,17 @@ public class AudioClass : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(gameObject.transform);
-        music = GameObject.Find("Music Source").GetComponent<AudioSource>();
+        music = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
     }
 
     public void PlayMusic()
     {
-        if (music.isPlaying) return;
-        music.Play();
+        if (!music.isPlaying)
+        {
+            music.Play();
+        }
+
+        return;
     }
 
     public void StopMusic()
