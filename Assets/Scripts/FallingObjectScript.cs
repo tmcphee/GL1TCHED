00472bb8359d07041133909a9d8843b69a505612
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2021-03-28 22:54:09 -0400

    Added Falling Object Generator

---
+++ Assets/Scripts/FallingObjectScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectScript : MonoBehaviour
+{
+    void Update()
+    {
+        /* Tyler McPhee
+         * Used for object cleanup
+         * When the object is out of the screen then detroy it
+         */
+        if (transform.position.y < -50)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
