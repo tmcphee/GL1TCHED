f861d5746baeda1dcd15d013fd639b165acdc3fc
aargreer <aagreer@lakeheadu.ca>
2021-04-11 19:11:24 -0400

    changed W3L1

--- Assets/Trampoline.cs
+++ Assets/Trampoline.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Andrew Greer:
+ *   - handles bouncing off of a trampoline object
+ */
 public class Trampoline : MonoBehaviour
 {
     public float bounciness;
-
     private Rigidbody2D player;
 
+    // finds the player object
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
+
+    // if the player collides, apply a force opposite to the collision, multiplied by bounciness and impact velocity
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
@@ -24,7 +29,6 @@ public class Trampoline : MonoBehaviour
                                                    bounciness * collision.relativeVelocity.magnitude * Time.deltaTime * -contact.normal[1]);
 
                 player.AddForce(bounceVector, ForceMode2D.Impulse);
-                Debug.Log(bounceVector);
             }
         }
     }
